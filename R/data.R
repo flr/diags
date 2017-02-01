fce5955cb240f49fcdb56f78f9b8279b6d53eb98
laurieKell <laurie@kell.es>
2017-02-01 13:34:40 +0100

    added catch/effort

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' ce
+#' 
+#' #' An example dataset with catch and effort by year, month, week and fleet based on North 
+#' Atlantic Albacore, derived from the ICCAT multifan-CL assessment 
+#'
+#' @format An \code{data.frame} object
+#' \describe{
+#'   \item{yr}{year, \code{numeric}}
+#'   \item{mo}{month, \code{numeric}}
+#'   \item{wk}{week, \code{numeric}}
+#'   \item{flt}{fleet, \code{numeric}}
+#'   \item{catch}{catch, \code{numeric}}
+#'   \item{effort}{effort, \code{numeric}}
+#'   ...
+#' }
+#' @name ce
+#' @docType data
+#' @keywords data
+NULL
