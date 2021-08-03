c8fa365c688e627cd120140654bd78b863bd84ff
lauriekell <laurie@kell.es>
2021-08-03 05:43:37 +0100

    order of values changed

--- R/diags-aav.R
+++ R/diags-aav.R
@@ -10,38 +10,37 @@ setGeneric('antiCurve', function(object, ...)
   standardGeneric('antiCurve'))
 
 avFn<-function(object){
-  o1 =object[-1]
-  o2 =object[-length(object)]
+  o1=object[-length(object)]
+  o2=object[-1]
   
-  return(abs(o1-o2)/o1)}
+  return(abs((o2-o1)/o1))}
 
 setMethod('aav', signature(object='FLQuant'),
           function(object){
           
-            dmns=dimnames(object)
-            dmns[[2]]=dmns[[2]][-length(dmns[[2]])]
-            dmns=dmns[c(TRUE,(laply(dmns[-1],length)>1))]
+  dmns=dimnames(object)
+  dmns[[2]]=dmns[[2]][-length(dmns[[2]])]
+  dmns=dmns[c(TRUE,(laply(dmns[-1],length)>1))]
             
-            res=aaply(object,c(1,3:6), avFn)
-            res=FLQuant(c(res),dimnames=dmns)
+  res=aaply(object,c(1,3:6), avFn)
+  res=FLQuant(c(res),dimnames=dmns)
           
-              res=aaply(res,c(1,3:6), mean)
-            res=FLQuant(c(res),dimnames=dmns[-2])
+  res=aaply(res,c(1,3:6), mean)
+  res=FLQuant(c(res),dimnames=dmns[-2])
             
-            return(res)})
-
+  return(res)})
 
 setMethod('av', signature(object='FLQuant'),
           function(object){
             
-            dmns=dimnames(object)
-            dmns[[2]]=dmns[[2]][-length(dmns[[2]])]
-            dmns=dmns[c(TRUE,(laply(dmns[-1],length)>1))]
+  dmns=dimnames(object)
+  dmns[[2]]=dmns[[2]][-length(dmns[[2]])]
+  dmns=dmns[c(TRUE,(laply(dmns[-1],length)>1))]
             
-            res=aaply(object,c(1,3:6), avFn)
-            res=FLQuant(c(res),dimnames=dmns)
+  res=aaply(object,c(1,3:6), avFn)
+  res=FLQuant(c(res),dimnames=dmns)
             
-            return(res)})
+  return(res)})
 
 setMethod('aav', signature(object='data.frame'),
  function(object){
